4c75cf4402cb04cf6776dc2b41e10503af02cf8a
Mark Nutter <marknutter@gmail.com>
2015-07-29 23:39:15 -0500

    voting and submitter tracking in place and working

--- app.js
+++ app.js
@@ -4,14 +4,36 @@ function LiveLinks(fbname) {
   this.firebase = firebase;
   var linksRef = firebase.child('links');
   var usersRef = firebase.child('users');
+  var votesRef = firebase.child('votes');
   var instance = this;
 
   this.submitLink = function(url, title) {
     url = url.substring(0,4) !== "http" ? "http://" + url : url;
-    linksRef.child(btoa(url)).set({
+    linksRef.child(btoa(url)).update({
       title: title
+    }, function(error) {
+      if (error) { 
+        instance.onError(error)
+      } else {
+        linksRef.child(btoa(url))
+                .child('users')
+                .child(instance.auth.uid)
+                .set(true)
+        usersRef.child(instance.auth.uid)
+                .child('links')
+                .child(btoa(url))
+                .set(true);
+        instance.vote(url, 1);
+      } 
     });
   };
+
+  this.vote = function(voteId, voteVal) {
+    linksRef.child(voteId)
+            .child('votes')
+            .child(instance.auth.uid)
+            .set(voteVal);
+  }
 
   this.login = function(email, password) {
   	firebase.authWithPassword({
@@ -46,10 +68,26 @@ function LiveLinks(fbname) {
   	firebase.unauth();
   };
 
+  function getSubmitters(linkId, userIds) {
+    if (userIds) {
+      $.each(userIds, function(userId) {
+        var linkUserRef = linksRef.child(linkId).child('users').child(userId);
+        linkUserRef.once('value', function(snapshot) {
+          usersRef.child(snapshot.key())
+                  .child('alias')
+                  .once('value', function(snapshot) {
+                    instance.onLinkUserAdded(linkId, snapshot.val());
+                  });
+        });
+      });
+    }
+  }
+
   // overrideable event functions
   this.onLogin = function(user) {};
   this.onLogout = function() {};
   this.onLinksChanged = function(links) {};
+  this.onLinkUserAdded = function(linkId, alias) {};
   this.onError = function(error) {};
 
 
@@ -58,6 +96,7 @@ function LiveLinks(fbname) {
 
 	  firebase.onAuth(function(authResponse) {
 	  	if (authResponse) {
+        instance.auth = authResponse;
 	  		usersRef.child(authResponse.uid).once('value', function(snapshot) {
 	  			instance.user = snapshot.val();
 	  			instance.onLogin(instance.user);
@@ -72,11 +111,20 @@ function LiveLinks(fbname) {
 	    var preparedLinks = [];
 	    for (var url in links) {
 	      if (links.hasOwnProperty(url)) {
+          var voteTotal = 0;
+          if (links[url].votes) {
+            $.each(links[url].votes, function(userId, val) {
+              voteTotal += val;
+            });
+          }
 	        preparedLinks.push({
 	          title: links[url].title,
-	          url: atob(url)
+	          url: atob(url),
+            id: url,
+            voteTotal: voteTotal
 	        })
-	      }
+        getSubmitters(url, links[url].users);  
+        }
 	    }
 	    instance.onLinksChanged(preparedLinks);
 	  });
@@ -107,9 +155,26 @@ $(document).ready(function() {
   ll.onLinksChanged = function(links) {
     $(".links-list").empty();
     links.map(function(link) {
-      var linkElement = "<li class='list-group-item'><a href='" + link.url + "'>" + link.title + "</a></li>";
+      var linkElement = "<li data-id='" + link.id + "' class='list-group-item'>"  + 
+                          "<span class='vote-total'>" + link.voteTotal + "</span>" +
+                          "<span class='glyphicon glyphicon-triangle-top up vote' data-val='1'></span>"   +
+                          "<span class='glyphicon glyphicon-triangle-bottom down vote' data-val='-1'></span>"   +
+                          "<a href='" + link.url + "'>" + link.title + "</a><br>" + 
+                          "<span class='submitters'>submitted by:</span>"         + 
+                        "</li>";
       $(".links-list").append(linkElement);
     });
+
+    $(".vote").click(function(event) {
+      ll.vote($(this).parent().data().id, $(this).data().val);
+    });
+  };
+
+  ll.onLinkUserAdded = function(linkId, alias) {
+    var submitters = $("[data-id='" + linkId + "'] span.submitters");
+    if (submitters.text().indexOf(alias) == -1) {
+      submitters.append(" " + alias);
+    }
   };
 
   ll.onLogin = function() {
@@ -154,6 +219,8 @@ $(document).ready(function() {
     }
     return false;
   });
+
+  
 
 
   ll.start();
